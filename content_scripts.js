ab7cd8a39bb927b3f6b128fd1cf5111ff4eb1a7f
patioglass <patiopatimon@gmail.com>
2021-05-21 00:05:06 +0900

    コネクションをはって情報を渡すように変更/Tverも対象に追加

--- content_scripts.js
+++ content_scripts.js
@@ -1,42 +1,79 @@
+const PAGE_TYPE_NAMES = {
+    "PrimeVideo": "PrimeVideo",
+    "Tver": "Tver"
+}
 let target;
-chrome.runtime.sendMessage({
-    name: "init",
-});
+let isAdjusTimerPage = false;
+// video要素の監視
+let watchVideo;
+
+// eventとコネクションをはる
+const port = chrome.runtime.connect({name: "event"});
+port.postMessage({status: "connect_init"});
+
 window.onload = () => {
-    const urlCheck = setInterval(() => {
-        // Prime Video判定
-        const currentPage = document.querySelector(".av-retail-m-nav-text-logo").text;
-        if (currentPage && currentPage === 'Prime Video') {
-            Array.from(document.querySelectorAll("a"), (e) => {
-                e.addEventListener("click", () => {
-                    const watchVideo = setInterval(() => {
-                        target = document.getElementsByClassName("atvwebplayersdk-timeindicator-text")[0];
+    // 右クリック禁止解除 
+    document.addEventListener("contextmenu",function(e){e.stopPropagation();},true);
+
+    // eventから受け取るイベント
+    port.onMessage.addListener((response) => {
+        console.log(response);
+        if (response.name === "create_adjustimer") {
+            urlCheck();
+            // adjusTimerが起動
+            isAdjusTimerPage = true;
+        } else if (response.name === "close_adjustimer") {
+            // adjusTimerが終了
+            isAdjusTimerPage = false;
+            clearInterval(watchVideo);
+        } else if (response.name === "sync_video_info") {
+            // 情報同期（タイトルやvideo要素の取得）
+            const videoTitle = getVideoTitle(response.pageType);
+
+            port.postMessage({
+                name: "update",
+                status: "set_video_info",
+                videoTitle: videoTitle
+            });
+            switch(response.pageType) {
+                case PAGE_TYPE_NAMES["PrimeVideo"]:
+                    watchVideo = setInterval(() => {
+                        const target = document.getElementsByClassName("atvwebplayersdk-timeindicator-text")[0];
                         if (target) {
                             clearInterval(watchVideo);
-                            startObserver(target);
+                            // observer起動
+                            startObserver(target, videoTitle);
                         }
                     }, 500);
-                })
-            })
-            clearInterval(urlCheck);
+                    break;
+                case PAGE_TYPE_NAMES["Tver"]:
+                    // 右クリック解除しないとAdjusTimerが起動できない
+                    document.addEventListener("contextmenu",function(e){e.stopPropagation();},true);
+                    watchVideo = setInterval(() => {
+                        const target = document.querySelector(".vjs-current-time");
+        
+                        if (target) {
+                            clearInterval(watchVideo);
+                            startObserver(target, videoTitle);
+                        }
+                    }, 500)
+                    break;
+                default:
+                    break;
+            }
         }
-    }, 1000);
-
+    });
 }
 
-function startObserver(target) {
-    // シーズンがある場合
-    const season = document.querySelector(".dv-node-dp-seasons")
-    ? document.querySelector(".dv-node-dp-seasons").querySelector("[for]").textContent
-    : "";
-    const videoTitle = document.getElementsByClassName("av-detail-section")[0].querySelector('[data-automation-id]').textContent + " " + season;
+function startObserver(target, videoTitle) {
+    const title = videoTitle;
     let observer = new MutationObserver(records => {
-        records.forEach(record => {
-            if (record.target.nextElementSibling) {
-                chrome.runtime.sendMessage({
+        records.forEach((record, index) => {
+            if (record.target && index === 0) {
+                port.postMessage({
                     name: "update",
-                    title: videoTitle,
-                    remainingTime: record.target.nextElementSibling.textContent,
+                    status: "time_update",
+                    title: title,
                     currentTime: record.target.textContent
                 }, (response) =>{});
             }
@@ -45,9 +82,62 @@ function startObserver(target) {
     if (target) {
         observer.observe(target, {
             characterData: true,
+            childList: true,
             subtree: true
         });
     } else {
         console.log("取得に失敗しました.");
     }
+}
+
+function urlCheck() {
+    const currentPageUrl = location.href;
+    if (currentPageUrl.match(/https:\/\/www.amazon.co.jp\/*/)) {
+        const urlCheckPrime = setInterval(() => {
+            // Prime Video判定
+            const currentPage = document.querySelector(".av-retail-m-nav-text-logo").text;
+            if (isAdjusTimerPage && currentPage && currentPage === "Prime Video") {
+                // Prime判定ができたことをeventに伝える
+                port.postMessage({
+                    name: "update",
+                    type: "init_page",
+                    pageType: PAGE_TYPE_NAMES["PrimeVideo"]
+                });
+                clearInterval(urlCheckPrime);
+            }
+        }, 1000);
+    } else if (currentPageUrl.match(/https:\/\/tver.jp\/corner\/*/)) {
+        const urlCheckTver = setInterval(() => {
+            // Tver判定
+            const title = document.querySelector(".vjs-dock-title");
+            if (isAdjusTimerPage && title && title.textContent) {
+                // Tver判定ができたことをeventに伝える
+                port.postMessage({
+                    name: "update",
+                    type: "init_page",
+                    pageType: PAGE_TYPE_NAMES["Tver"]
+                });
+                clearInterval(urlCheckTver);
+            }
+        }, 1000);
+    }
+}
+
+function getVideoTitle(pageType) {
+    let videoTitle = "";
+    switch(pageType) {
+        case PAGE_TYPE_NAMES["PrimeVideo"]:
+            // シーズンがある場合（prime videoのみ）
+            const season = document.querySelector(".dv-node-dp-seasons")
+                ? document.querySelector(".dv-node-dp-seasons").querySelector("[for]").textContent
+                : "";
+            videoTitle = document.getElementsByClassName("av-detail-section")[0].querySelector("[data-automation-id]").textContent + " " + season;
+            break;
+        case PAGE_TYPE_NAMES["Tver"]:
+            videoTitle = document.querySelector(".vjs-dock-title").textContent;
+            break;
+        default:
+            break;
+    }
+    return videoTitle;
 }
--- event.js
+++ event.js
@@ -1,10 +1,12 @@
 let adjusTimerWindow;
+let connectionPort;
+
 chrome.runtime.onInstalled.addListener(function(){
     // メニューを生成
     const parent_menu = chrome.contextMenus.create({
-      type: "normal",
-      id: "parent",
-      title: "AdjusTimer(アジャスタイマー)"
+        type: "normal",
+        id: "parent",
+        title: "AdjusTimer(アジャスタイマー)"
     });
 });
 chrome.contextMenus.onClicked.addListener(function(item){
@@ -13,14 +15,24 @@ chrome.contextMenus.onClicked.addListener(function(item){
         "adjustimer",
         "width=1000, height=700"
     );
+    if (connectionPort) {
+        // adjusTimer起動をcontent_scriptに送信
+        connectionPort.postMessage({
+            name: "create_adjustimer"
+        });
+    }
 });
 
-// Receive message from content script
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.name === "update") {
-        updateTimerDom(request);
-    }
-    return true;
+// content_scriptとコネクションをはる
+chrome.runtime.onConnect.addListener((port) => {
+    // init_connection時にポートを記録する
+    connectionPort = port;
+    // content_scriptからpostを受け取る
+    port.onMessage.addListener((request) => {
+        if (request.name === "update") {
+            updateTimerDom(request);
+        }
+    })
 });
 
 /**
@@ -35,13 +47,40 @@ function updateTimerDom(update) {
     if (!adjusTimerWindow) {
         return;
     }
-    const alertDom = adjusTimerWindow.document.getElementById("alert"); // 未取得時の文
-    const videoTitleDom = adjusTimerWindow.document.getElementById("video__title"); // ビデオタイトル
-    const currentTimeDom = adjusTimerWindow.document.getElementById("video__time_current"); // 現在時間
-    // const remainingTimeDom = adjusTimerWindow.document.getElementById("video__time_remaining"); // 残り時間
 
-    alertDom.innerText = "";
-    videoTitleDom.innerText = update.title;
-    currentTimeDom.innerText = update.currentTime;
-    // remainingTimeDom.innerText = update.remainingTime;
+    if (update.type === "init_page") {
+        const syncButtonDom = adjusTimerWindow.document.querySelector(".button__sync");
+        syncButtonDom.setAttribute("id", "button__get_video_info");
+        const currentPageDom = adjusTimerWindow.document.querySelector("#current_page_name");
+        currentPageDom.innerText = update.pageType;
+
+        // 情報取得ボタンを押すことでcontent_scriptから情報をもらうようにする
+        syncButtonDom.removeEventListener("click", syncVideoInfo);
+        syncButtonDom.addEventListener("click", syncVideoInfo, false);
+        syncButtonDom.port = connectionPort;
+        syncButtonDom.pageType = update.pageType;
+
+    } else if (update.status === "set_video_info") {
+        const videoTitleDom = adjusTimerWindow.document.getElementById("video__title"); // ビデオタイトル
+        const currentTimeDom = adjusTimerWindow.document.getElementById("video__time_current"); // 現在時間
+
+        videoTitleDom.innerText = update.videoTitle;
+        currentTimeDom.innerText = "00:00";
+        adjusTimerWindow.document.getElementById("remaining_time_wrapper").style.display = "block";
+
+    } else if (update.status === "time_update") {
+        const videoTitleDom = adjusTimerWindow.document.getElementById("video__title"); // ビデオタイトル
+        const currentTimeDom = adjusTimerWindow.document.getElementById("video__time_current"); // 現在時間
+
+        videoTitleDom.innerText = update.title;
+        currentTimeDom.innerText = update.currentTime;
+    }
+
+}
+
+function syncVideoInfo(e) {
+    e.target.port.postMessage({
+        name: "sync_video_info",
+        pageType: e.target.pageType
+    })
 }
