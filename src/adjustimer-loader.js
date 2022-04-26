f9560302612b311247c83d775709a4f8a9b76a75
patioglass <patiopatimon@gmail.com>
2022-04-26 23:12:57 +0900

    Netflixのシーズン情報も表示するように修正

--- src/adjustimer-loader.js
+++ src/adjustimer-loader.js
@@ -2,17 +2,36 @@ console.log("connect Netflix");
 const setTitle = () => {
   // タイトル取得
   let checkNetflix = setInterval(() => {
-    const title = window.netflix.appContext.state.playerApp.getState().videoPlayer.videoMetadata[location.href.split("?")[0].split("/").slice(-1)[0]]?._video?._video?.title;
-    if (title) {
+    console.log("change video");
+    const videoId = location.href.split("?")[0].split("/").slice(-1)[0];
+    const currentTitle = window.netflix.appContext.state.playerApp.getState().videoPlayer.videoMetadata[videoId]?._video?._video?.title;
+    const currentEpisodeId = window.netflix.appContext.state.playerApp.getState().videoPlayer.videoMetadata[videoId]?._video?._video?.currentEpisode;
+    if (currentTitle) {
+      let adjustimerTitle = `${currentTitle}`;
+      if (currentEpisodeId) {
+        let currentEpisodeIndex;
+        if (window.netflix.appContext.state.playerApp.getState().videoPlayer.videoMetadata[videoId]?._video?._video?.seasons.length > 0) {
+          const currentSeason = window.netflix.appContext.state.playerApp.getState().videoPlayer.videoMetadata[videoId]?._video?._video.seasons.filter(season => {
+            const episodeIndex = season.episodes.findIndex(episode => episode.episodeId === currentEpisodeId);
+            if (episodeIndex !== -1) {
+              currentEpisodeIndex = episodeIndex;
+              return season;
+            }
+          })
+          if (currentSeason) {
+            adjustimerTitle += ` シーズン${currentSeason[0].seq}-${currentSeason[0]?.episodes[currentEpisodeIndex]?.seq} ${currentSeason[0]?.episodes[currentEpisodeIndex]?.title}`
+          }
+        }
+      }
       if (document.querySelector(".netflixTitle")) {
-        document.querySelector(".netflixTitle").textContent = title;
+        document.querySelector(".netflixTitle").textContent = adjustimerTitle;
         clearInterval(checkNetflix);
       } else {
         // DOMに反映
         const titleNameElement = document.createElement("p");
         titleNameElement.style.display = "none";
         titleNameElement.className = "netflixTitle";
-        titleNameElement.textContent = title;
+        titleNameElement.textContent = adjustimerTitle;
         document.body.appendChild(titleNameElement);
         clearInterval(checkNetflix);
       }
@@ -21,22 +40,12 @@ const setTitle = () => {
 }
 
 setTitle();
-
-let oldHref = "";
-const bodyList = document.querySelector("head")
+let href = location.href;
 const observer = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutation) {
-    console.log("change");
-    if (oldHref != document.location.href) {
-      oldHref = document.location.href;
-      setTitle();
-    }
-  });
+  if(href !== location.href) {
+    href = location.href;
+    setTitle();
+  }
 });
 
-const config = {
-    childList: true,
-    subtree: true
-};
-observer.observe(bodyList, config);
-
+observer.observe(document, { childList: true, subtree: true });
