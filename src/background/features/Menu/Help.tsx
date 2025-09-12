8dd55f316ba646a9dbad32fb42d2dfe2e901c6ca
patioglass <patiopatimon@gmail.com>
2025-09-12 09:41:15 +0900

    ✏️ドキュメント整理

--- src/background/features/Menu/Help.tsx
+++ src/background/features/Menu/Help.tsx
@@ -1,19 +1,14 @@
+import imgUrl from "../../../../public/extension_48.png";
+
 const Help = () => {
     return (
-        <div className="text-left p-3 bg-gray-100">
-            <div>
-                <p># 更新履歴</p>
-                <ul className="list-disc list-inside">
-                    <li>デザインを実装した</li>
-                    <li>service workerからの起動を実装</li>
-                    <li>viteの環境を作成</li>
-                </ul>
-                <a href="" className="text-rose-400 border-b">⇒過去の更新履歴</a>
-            </div>
-            <div className="mt-5">
+        <div className="p-3 bg-gray-100">
+            <img className="mx-auto" src={imgUrl} />
+            <div className="text-left mt-5">
                 <p># 使い方</p>
                 <ul className="list-decimal list-inside">
-                    <li>対象の動画ページを選択する</li>
+                    <li>対象の動画ページを開く</li>
+                    <li>右のメニューから対象の動画ページを選択する</li>
                     <li>動画URLと一致しているかを確認する</li>
                     <li>色のパレット部分を選択し、UIを好みに合わせる</li>
                     <li>「情報を取得する」を押す</li>
