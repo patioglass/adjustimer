9cba08ae6468b1fe8b3695789e03dd8e6b0787d2
patioglass <patiopatimon@gmail.com>
2025-09-30 22:01:21 +0900

    ✅HashRoterに変更

--- lp/main.tsx
+++ lp/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/adjustimer/">
+    <HashRouter basename="/adjustimer/">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
