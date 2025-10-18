33462f3a95adfa73556a6ae4aa21d863e0df3c6f
patioglass <patiopatimon@gmail.com>
2025-10-19 03:46:29 +0900

    ✅オプションの初期値設定修正/画面幅調整

--- src/background/features/AdjusTimerWindow.tsx
+++ src/background/features/AdjusTimerWindow.tsx
@@ -51,14 +51,18 @@ export const AdjusTimerWindow = (): ReactElement => {
     useEffect(() => {
         // カラーピッカーの情報を取得
         // フォントの情報を取得
-        chrome.storage.local.get([STORAGE_KEY_BACKGROUND_COLOR, STORAGE_KEY_TEXT_COLOR, STORAGE_KEY_FONTFAMILIY, STORAGE_KEY_SHADOW_SIZE, STORAGE_KEY_SHADOW_COLOR, STORAGE_KEY_FONT_WEIGHT, STORAGE_KEY_IS_SHOW_DATE], (value) => {
+        chrome.storage.local.get(["hoge", STORAGE_KEY_BACKGROUND_COLOR, STORAGE_KEY_TEXT_COLOR, STORAGE_KEY_FONTFAMILIY, STORAGE_KEY_SHADOW_SIZE, STORAGE_KEY_SHADOW_COLOR, STORAGE_KEY_FONT_WEIGHT, STORAGE_KEY_IS_SHOW_DATE], (value) => {
             setBackgroundColor(value[STORAGE_KEY_BACKGROUND_COLOR]);
             setTextColor(value[STORAGE_KEY_TEXT_COLOR]);
             setCustomFont(value[STORAGE_KEY_FONTFAMILIY]);
             setShadowSize(value[STORAGE_KEY_SHADOW_SIZE]);
             setShadowColor(value[STORAGE_KEY_SHADOW_COLOR]);
             setFontWeight(value[STORAGE_KEY_FONT_WEIGHT]);
-            setShowCurrentDate(value[STORAGE_KEY_IS_SHOW_DATE]);
+            if (value[STORAGE_KEY_IS_SHOW_DATE] === undefined) {
+                setShowCurrentDate(true);
+            } else {
+                setShowCurrentDate(value[STORAGE_KEY_IS_SHOW_DATE]);
+            }
         });
     }, [])
 
