44321d840f19ef6b57c1bb12a311238d738d11cf
patioglass <patiopatimon@gmail.com>
2025-11-28 07:34:05 +0900

    ⚙️OBS設項目定追加

--- lp/components/Home.tsx
+++ lp/components/Home.tsx
@@ -9,6 +9,9 @@ import tverLogo from '../../public/tver-logo.png';
 import unextLogo from '../../public/u-next-logo.png';
 import demo1 from '../../public/readme_image01.jpg'
 import demo2 from '../../public/readme_image02.jpg'
+import obsSetting1 from '../../public/obs-setting-01.jpg'
+import obsSetting2 from '../../public/obs-setting-02.jpg'
+import obsSetting3 from '../../public/obs-setting-03.jpg'
 import { ReactElement } from 'react';
 import Footer from './Footer';
 
@@ -79,19 +82,39 @@ const Home =  (): ReactElement => {
                 <h3 className="text-xl font-semibold mb-2">Step 3</h3>
                 <p>AdjusTimerの右側メニュー　(　【- 対象にするページ -】　)　から動画再生タブを選択</p>
             </div>
-            <img src={demo1} className="w-1/2 m-auto" />
-
             <div className="bg-gray-800 p-6 rounded-sm">
                 <h3 className="text-xl font-semibold mb-2">Step 4</h3>
                 <p>「情報を取得する」を押す</p>
             </div>
-            <br />
-            <div className="bg-gray-900 p-6 rounded-sm">
-                <h3 className="text-xl font-semibold mb-2">その他（OBSの設定等）</h3>
-                <p>OBSの設定等、詳細は動画をご確認ください。</p>
+            <img src={demo1} className="w-1/2 m-auto" />
+
+        </section>
+
+        {/* OBS */}
+        <h2 className="text-3xl font-bold text-center mt-20" id="obs-setting">
+            <p>OBSの設定</p>
+            <p className="text-xs">- obs setting -</p>
+        </h2>
+         <section className="py-20 px-8 lg:w-3/5 m-auto">
+            <div className="mb-20">
+                <h3 className="text-xl font-semibold mb-2">ウィンドウキャプチャのやり方</h3>
+                <p>1. 「ソース追加」⇒「ウィンドウキャプチャ」</p>
+                <p>2. AdjusTimerの「ポップアップを表示させる」（ピンクのボタン）を押す</p>
+                <p>3. 「AdjusTimer -ポップアウト-」があるので、「AdjusTimer -ポップアウト-」を選ぶ。</p>
+                <img src={obsSetting1} className="w-1/2 m-auto" />
+                <br />
+                <p>4. 「AdjusTimer -ポップアウト-」があるので、「AdjusTimer -ポップアウト-」を選ぶ。</p>
+                <p>5. 「ウィンドウのタイトルに一致する必要があります」を選ぶ。</p>
+                <img src={obsSetting2} className="w-1/2 m-auto" />
+                <br />
+                この設定で、OBSの立ち上げ時にポップアウトのウィンドウを使ったキャプチャ配信が可能となります。
             </div>
-            <br />
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/VYC_IGwp_bc?si=NbJbNQmu0IjkKx59&amp;start=151" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+            <div className="mb-20">
+                <h3 className="text-xl font-semibold mb-2">クロマキー(背景透明)設定</h3>
+                <p>さきほど追加した「ウィンドウキャプチャ」を右クリック⇒「フィルタ」⇒「クロマキー」を追加</p>
+                <p>デフォルトだと緑が透明対象のため、AdjusTimer側の背景色を緑にする</p>
+                <img src={obsSetting3} className="w-1/2 m-auto" />
+            </div>
         </section>
 
         {/* Q&A */}
@@ -113,8 +136,13 @@ const Home =  (): ReactElement => {
                 <h3 className="text-xl font-semibold mb-2">その他の不具合</h3>
                 <p>調査させていただきますので、<a href="https://forms.gle/gyDZ7cF7m8mcoNVC9" target="_blank" className="underline text-cyan-400">こちらのフォーム</a>に報告いただけると幸いです。</p>
             </div>
+            <h2 className="text-3xl font-bold text-center mt-20">
+                <p>動画</p>
+                <p className="text-xs">- video -</p>
+            </h2>
+            <br />
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/VYC_IGwp_bc?si=NbJbNQmu0IjkKx59&amp;start=151" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         </section>
-
         {/* 連絡先 */}
         <h2 className="text-3xl font-bold text-center mt-20">
             <p>連絡先</p>
