7cb2b282029342466e8d01c66c2d61c2ada88e86
patioglass <patiopatimon@gmail.com>
2025-11-28 14:54:46 +0900

    ⚙️OBSの設定追加

--- lp/App.tsx
+++ lp/App.tsx
@@ -3,14 +3,20 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import TermsOfUse from './components/TermsOfUse';
 import Policy from './components/Policy';
+import ObsSetting from './components/ObsSetting';
+import ScrollToTop from './components/ScrollTop';
 
 const App =  (): ReactElement => {
   return (
+    <>
+    <ScrollToTop />
     <Routes>
       <Route path='/' element={<Home/>} />
+      <Route path='/obs-setting' element={<ObsSetting/>} />
       <Route path='/terms-of-use' element={<TermsOfUse/>} />
       <Route path='/privacy-policy' element={<Policy/>} />
     </Routes>
+    </>
   );
 }
 
